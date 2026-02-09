a63b7976dd21bee70b673258fef5e6c098dee76d
Anshul Agrawal <anshul.a.agrawal@oracle.com>
2026-02-09 21:37:54 +0530

    feat(12-01): create SettingsActivity with toolbar and back navigation
    - Create SettingsActivity with MaterialToolbar showing "Settings" title
    - Add back arrow navigation via setDisplayHomeAsUpEnabled
    - Create activity_settings.xml with toolbar and FrameLayout container
    - Register SettingsActivity in AndroidManifest with parent MainActivity
    - FrameLayout container ready for PreferenceFragment in Plan 02

---
+++ app/src/main/java/com/ava/notiva/SettingsActivity.java
@@ -0,0 +1,28 @@
+package com.ava.notiva;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
+import dagger.hilt.android.AndroidEntryPoint;
+
+@AndroidEntryPoint
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+
+        Toolbar settingsToolbar = findViewById(R.id.settings_toolbar);
+        setSupportActionBar(settingsToolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onSupportNavigateUp() {
+        getOnBackPressedDispatcher().onBackPressed();
+        return true;
+    }
+}
